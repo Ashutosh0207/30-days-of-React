6d8e97f433c6c085df61031993ebd4a858e03b3a
Ashutosh0207 <94388293+Ashutosh0207@users.noreply.github.com>
2023-05-19 00:42:31 +0530

    Add files via upload

---
+++ Day-4/task1/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Bar from './components/bar';
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h2 className='txt'>Front End Technoogies</h2>
+      <Bar />
+    </>
+  );
+}
+
+export default App;
